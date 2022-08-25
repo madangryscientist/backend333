38710926336c09e46a9dea879d1f498340b038d6
Erin <moon4elf20@gmail.com>
2022-08-25 17:30:25 +0200

    faq edited

---
+++ backend333/Migrations/20220825121008_faqs.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace backend333.Migrations
+{
+    public partial class faqs : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Faqs",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Faqs",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Faqs");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Faqs");
+        }
+    }
+}
